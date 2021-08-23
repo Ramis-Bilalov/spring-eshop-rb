750623e2dba67329328a69d8a4faa9d046e86bb3
Ramis Bialov <ramiska81@gmail.com>
2021-08-23 20:04:57 +0300

    Классная работа №2

--- src/main/java/com/bilalov/springeshoprb/controllers/BucketController.java
+++ src/main/java/com/bilalov/springeshoprb/controllers/BucketController.java
@@ -5,8 +5,6 @@ import com.bilalov.springeshoprb.service.BucketService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.security.Principal;
 
 @Controller
